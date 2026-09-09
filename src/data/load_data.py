6053920b6cce7428c312b8bccf4ecd095dd5e1a4
Tapaswini005 <shawtapaswini2005@gmail.com>
2026-09-09 20:44:14 +0530

    Rewrite correct code

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 df = pd.read_csv(
     r'C:\Users\Tapaswini Shaw\OneDrive\Desktop\Ecommerce_Order_Cancellation_Prediction\data\raw\orders.csv',
-    encoding='utf-8',
+    sep=';',          # ✅ tells pandas to use semicolons
+    encoding='utf-8', # or 'latin1' if utf-8 fails
     low_memory=False
 )
-
 print(df.head())
