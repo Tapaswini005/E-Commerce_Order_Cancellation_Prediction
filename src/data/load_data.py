eab45b4d8f387ab86929c3f200f3ac00feb2d008
Tapaswini005 <shawtapaswini2005@gmail.com>
2026-09-09 21:16:28 +0530

    Modify load_data.py  code

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,9 +1,22 @@
 import pandas as pd
 
-df = pd.read_csv(
-    r'C:\Users\Tapaswini Shaw\OneDrive\Desktop\Ecommerce_Order_Cancellation_Prediction\data\raw\orders.csv',
-    sep=';',          # ✅ tells pandas to use semicolons
-    encoding='utf-8', # or 'latin1' if utf-8 fails
-    low_memory=False
-)
-print(df.head())
+def load_data(file_path):
+    df = pd.read_csv(
+        file_path,
+        sep = ";",
+        encoding = "utf-8",
+        low_memory = False
+    )
+    
+    return df
+
+if __name__ == "__main__":
+    file_path = r"C:\Users\Tapaswini Shaw\OneDrive\Desktop\Ecommerce_Order_Cancellation_Prediction\data\raw\orders.csv"
+    
+    df = load_data(file_path)
+    
+    print("Dataset loaded successfully!")
+    print("Shape:", df.shape)
+    print("\nFirst 5 rows:")
+    print(df.head())
+       
