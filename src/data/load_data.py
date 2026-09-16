ba512d54421ade067e65e7284b9e977b3495902b
Tapaswini005 <shawtapaswini2005@gmail.com>
2026-09-16 23:35:41 +0530

    Adding new dataset

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,2 +1,32 @@
-#Data is not loaded in this file
-#It will load data from the data folder.
+import pandas as pd
+
+
+def load_data(file_path):
+    df = pd.read_csv(
+        file_path,
+        encoding="utf-8",
+        low_memory=False
+    )
+
+    return df
+
+
+if __name__ == "__main__":
+    file_path = r"C:\Users\Tapaswini Shaw\OneDrive\Desktop\Ecommerce_Order_Cancellation_Prediction\data\raw\df_train.csv"
+
+    df = load_data(file_path)
+
+    print("Dataset loaded successfully!")
+    print("Shape:", df.shape)
+
+    print("\nFirst 5 rows:")
+    print(df.head())
+
+    print("\nColumns:")
+    print(df.columns.tolist())
+
+    print("\nDataset information:")
+    print(df.info())
+
+    print("\nMissing values:")
+    print(df.isnull().sum())
